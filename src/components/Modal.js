533e7a2b111f929a94bbddcfda7a7c2c48d1b732
Tallia-Anne <erinawambiekele@gmail.com>
2022-12-01 17:45:52 +0100

    modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,9 +4,9 @@ const Modal = ({onClose, results, data}) => {
         <div className="modal is-active">
             {/* permet de fermet la modal */}
             <div className="modal-background" onClick={onClose}></div>
-            <div className="model-card">
-                <header className="model-card-head">
-                    <p className="modal-card-title">Your answers</p>
+            <div className="modal-card">
+                <header className="modal-card-head">
+                    <p className="modal-card-title">Tes réponses:</p>
                     {/* permet de fermet la modal */}
                     <button className="delete" onClick={onClose}></button>
                 </header>
@@ -18,9 +18,9 @@ const Modal = ({onClose, results, data}) => {
                                 {/* les question */}
                                 <p><strong>{result.q}</strong></p>
                                 {/* la reponse qu'on selectionner qui est faux */}
-                                <p className={result.a === data[i].answer ? 'has-background-success has-text-white p-2' : 'has-background-danger has-text-white p-2'}>Your answer: {result.a}</p>
+                                <p className={result.a === data[i].answer ? 'has-background-success has-text-white p-2' : 'has-background-danger has-text-white p-2'}>La réponse: {result.a}</p>
                                 {/* la reponse qu'on selectionner qui est vrai */}
-                                {result.a !== data[i].answer && <p className="has-background-link has-text-white p-2">Correct answer: {data[i].answer}</p>}
+                                {result.a !== data[i].answer && <p className="has-background-link has-text-white p-2">Les bonnes réponses: {data[i].answer}</p>}
                             </li>
                         ))}
                     </ul>
